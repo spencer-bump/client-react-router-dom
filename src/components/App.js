aecb68e5d05f4383ffceaaf8b561a309eb880551
Spencer Eldred <eldredspencer@gmail.com>
2019-08-15 10:16:54 -1000

    Add react-router-dom BrowserRouter and Route

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// react-router-dom
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>Click Me!</button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
